c50646095661d0ebdd5a78b4711d584767635f38
Rodrigo Martins <policante.martins@gmail.com>
2020-11-05 16:05:00 -0300

    Initial Commit

---
+++ Dictionary_RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dictionary_RickAndMorty
+//
+//  Created by Rodrigo Martins on 05/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
